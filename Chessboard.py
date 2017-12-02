f4a27f4c6f3ea94e034646af8944577fa148d4a7
Jc <jeow9028@colorado.edu>
2017-12-02 13:11:44 -0700

    Finished the main

--- Chessboard.py
+++ Chessboard.py
@@ -15,59 +15,76 @@ def board ():
 #  [0, 0, 0, 0, 0, 0, 0, 0],
 # 
 
-	z = np.random.randint(64, size=(8, 8))
-	print z	()
+z = np.random.randint(64, size=(8, 8))
+print z	()
 	int x = 1;		#White space
 	int y = x+1;	#Black space
 
-def turn():
-	# Makea function for the turn you have if you are a white piece or black piece
-	# if turn is 0 then white pice starts
-
-	white = 0
-	black  = 0
-
-	turn = 0
-	turn = turn +1
-	if (turn == 0)
-	
-
-def movement():
+	def movement():
 
 
 
-def main ():
+		def main ():
 
-	"""Main function that will welcome the player to the game."""
+			"""Main function that will welcome the player to the game."""
 
-    print("\tWelcome to Chess Slaughter house! This is a turn based Chess simulator where")
-    print("\tTeam white or team black shall vanquish")
+			print("\tWelcome to Chess Slaughter house! This is a turn based Chess simulator where")
+			print("\tTeam white or team black shall vanquish")
 
-    print("\nHow to play.\n\nPlayers take turn to choose a move.")
-    print("Choose tou piece and hope to win")
-    print("(Still in Beta)")
+			print("\nHow to play.\n\nPlayers take turn to choose a move.")
+			print("Choose tou piece and hope to win")
+			print("(Still in Beta)")
 
-    print("\nThat's it! Good luck")
+			print("\nThat's it! Good luck")
 
-    """#################################"""
-    while play:
+			"""#################################"""
+# Calls and initalize the function for making the board
+board()
+play = True
+while play:
 
-	    turn  = random.randint(1,2)
-	    if turn ==1:
-	    	white  = True
-	    	black = False
-	    	print ("\n White player will commence!")
-	    else
-	    	white = False
-	    	black = True
-	    	print ("\n White player will commence!")
+	# Plays with the turn
+	turn  = random.randint(1,2)
+	if turn ==1:
+		white  = True
+		black = False
+		print ("\n White player will commence!")
+		else
+		white = False
+		black = True
+		print ("\n White player will commence!")
+	# Choose player
+
+	try:
+		Player_white = raw_input('w')
+		except
+		player_black = raw_input('b')
+
+	# Switch turns
+	white = not black
+	black = not white
 
 
+		print("\nWhite Player: ", white, "Computer health: ", black)
+		print("\nCongratulations! You have won. You're an animal!!")
+	else:
+		print("\nPlayer health: ", white , "Computer health: ", black)
 
-	board()
-	turn()
-	movement()
-	# Calls Functions for 
-	# Chess board / pieces
-	# Matrix 8x8
+		print("\nWould you like to play again?")
+
+		 answer = input("> ").lower()
+        if answer not in ("yes", "y"):
+            play_again = False
+
+main()
+		else
+		print("\nThank you for playing!!")
+		exit = raw_input('q')
+		play = False
+		exit = play
+main()
+
+		# Calls Functions for 
+		# Chess board / pieces
+		# Matrix 8x8
 
